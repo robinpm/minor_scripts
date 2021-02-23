c48cf1433a92131906f486cf784d3b05671f858e
Robin PM <dev@robinpm.com>
2021-02-23 14:46:01 -0500

    Added PilotPro.bat from previous repository

---
+++ PilotPro.bat
@@ -0,0 +1,7 @@
+:: Script to easily start Microsoft Flight Simulator 2020, FSUIPC, and Landing Rate Monitor
+@echo off
+start "" steam://rungameid/1250410
+start "" "C:\FSUIPC7\FSUIPC7.exe"
+start "" "C:\Program Files (x86)\Landing Rate Monitor\LRM.exe"
+
+
